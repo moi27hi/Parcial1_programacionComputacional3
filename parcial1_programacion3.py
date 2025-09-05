1e1449f7af4396fb0295762a998e58c6861d1d43
MoisesRamirez-ugb <abelinomoises@gmail.com>
2025-09-05 07:28:15 -0600

    Creacion de registro ventas

--- parcial1_programacion3.py
+++ parcial1_programacion3.py
@@ -2,5 +2,17 @@
 
 class ventas:
 
-    def __init__(self):
-        pass
+    def __init__(self,nombre_producto,precio_unitario):
+        self.nombre_producto = nombre_producto
+        self.precio_unitario = precio_unitario
+
+        venta = []
+
+
+    def registrar_venta(self):
+         self.nombre_producto = input("Ingrese el nombre del producto")
+
+         
+
+    
+    
